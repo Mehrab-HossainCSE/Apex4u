f7844d526181764b5b755bd32d0a5a70f5b1fa21
emonc <emonc@DESKTOP-L392UFS>
2024-05-09 21:48:44 +0600

    Add project files.

---
+++ Apex4u.Persistence/Data/EFDataContext.cs
@@ -0,0 +1,22 @@
+﻿using Apex4u.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apex4u.Persistence.Data
+{
+    public class EFDataContext : DbContext
+    {
+        public EFDataContext(DbContextOptions<EFDataContext> options) : base(options) { }
+        DbSet<Product> Products { get; set; }
+        DbSet<Stock> Stocks { get; set; }
+        DbSet<Warehouse> Warehouses { get; set; }
+        DbSet<Variant> Variants { get; set; }
+        
+        public override 
+
+    }
+}
---
+++ Apex4u.Persistence/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apex4u.Persistence.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; } // Primary Key
+        public string Name { get; set; } // Search engine friendly name
+        public string Description { get; set; } // Optional
+        public DateTime CreatedOn { get; set; }
+        public DateTime? UpdatedOn { get; set; } // Optional
+
+        // Navigation property for variants
+        public ICollection<Variant> Variants { get; set; }
+    }
+}
